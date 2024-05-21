63e02b52ec1b61aad27db96f578ac36a393dadd6
SamekPiotr <231030@student.uek.krakow.pl>
2024-05-21 23:03:49 +0200

    2024-05-21 PS

--- app/routes.py
+++ app/routes.py
@@ -12,8 +12,8 @@ def extract():
     if request.method == 'POST':
         product_id = request.form.get("product_id") 
         url = f"https://www.ceneo.pl/{product_id}"
-        response = request.get(url)
-        if response.status_code == requests.status_codes["ok"]:
+        response = requests.get(url)
+        if response.status_code == requests.codes["ok"]:
             page_dom = BeautifulSoup(response.text, "html.parser")
             try:
                 opinions_count = page_dom.select_one('a.product-review__link > span').text.strip()
@@ -22,7 +22,7 @@ def extract():
             if opinions_count:
                 #extraction process
                 return redirect(url_for('product', product_id=product_id))
-            return render_template("extract.html", error = "Product does not exist.")
+            return render_template("extract.html", error = "Product has no opinions")
         return render_template("extract.html", error="Product does not exist")
     return render_template("extract.html")
 
@@ -36,4 +36,4 @@ def author():
 
 @app.route("/product/<product_id>")
 def product(product_id):
-    return render_template("product.htm", product_id=product_id)
+    return render_template("product.html", product_id=product_id)
