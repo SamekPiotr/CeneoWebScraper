c1a031e11ad78928ee20369a535aea101e349076
SamekPiotr <231030@student.uek.krakow.pl>
2024-05-27 22:57:10 +0200

    2024-05-27 PS

--- app/routes.py
+++ app/routes.py
@@ -12,63 +12,61 @@ import numpy as np
 def index():
     return render_template("index.html")
 
-@app.route("/extract", methods=['POST', "GET"])
+@app.route('/extract', methods=['POST', 'GET'])
 def extract():
     if request.method == 'POST':
-        product_id = request.form.get("product_id") 
+        product_id = request.form.get('product_id')
         url = f"https://www.ceneo.pl/{product_id}"
         response = requests.get(url)
-        if response.status_code == requests.codes["ok"]:
+        if response.status_code == requests.codes['ok']:
             page_dom = BeautifulSoup(response.text, "html.parser")
-            product_name = extract_content(page_dom, "h1")
-            opinions_count = extract_content(page_dom,"a.product-review_link > span")                
+            opinions_count = extract_content(page_dom, 'a.product-review__link > span')
             if opinions_count:
-                url=f"https://www.ceneo.pl/{product_id}#tab-reviews"
                 all_opinions = []
+                url = f"https://www.ceneo.pl/{product_id}#tab=reviews"
                 while(url):
                     response = requests.get(url)
                     response.status_code
-                    page_dom = BeautifulSoup(response.text,"html.parser")
-                    opinions =page_dom.select("div.js_product-review")
+                    page_dom = BeautifulSoup(response.text, "html.parser")
+                    opinions = page_dom.select('div.js_product-review')
                     for opinion in opinions:
-                        single_opinion={
-                            key: extract_content(opinion, *value)
-                                for key,value in selectors.items()
+                        single_opinion = {
+                                key: extract_content(opinion, *value)
+                                    for key, value in selectors.items()
                         }
-                        for key,value in transformations.items():
+                        for key, value in transformations.items():
                             single_opinion[key] = value(single_opinion[key])
                         all_opinions.append(single_opinion)
-                    try:
-                        url = "https://www.ceneo.pl"+extract_content(page_dom, "a.pagination__next", "href")
+                    try:    
+                        url = "https://www.ceneo.pl"+extract_content(page_dom,"a.pagination__next", "href")
                     except TypeError:
-                        url = None
+                        url = None   
                 if not os.path.exists("app/data"):
                     os.mkdir("app/data")
                 if not os.path.exists("app/data/opinions"):
                     os.mkdir("app/data/opinions")
-                with open(f"app/data/opinions/{product_id}.json","w",encoding="UTF-8") as jf:
-                    json.dump(all_opinions, jf, indent=4, ensure_ascii = False)
+                with open(f"app/data/opinions/{product_id}.json", "w", encoding="UTF-8") as jf:
+                    json.dump(all_opinions, jf, indent=4, ensure_ascii=False)
                 MAX_SCORE = 5
                 opinions = pd.DataFrame.from_dict(all_opinions)
-                opinions.stars = opinions.stars.apply(lambda s: round(s*MAX_SCORE,1))
-                opinions_count = opinions.shape[0]
-                statistics={
+                opinions.score = opinions.stars.apply(lambda s: round(s*MAX_SCORE,1))
+                statistics = {
                     'product_id' : product_id,
-                    'product_name' : product_name,
+                    'product_name' : extract_content(page_dom, "h1"),
                     'opinions_count' : opinions_count,
                     'pros_count' : int(opinions.pros.astype(bool).sum()),
                     'cons_count' : int(opinions.cons.astype(bool).sum()),
                     'average_score' : opinions.stars.mean().round(3),
                     'score_distribution' : opinions.stars.value_counts().reindex(np.arange(0.5,5.5,0.5)).to_dict(),
-                    'recommendation_distribution' : opinions.authors_recommendation.value_counts(dropna=False).reindex([1,np.nan,0]),
+                    'recommendation_distribution' : opinions.authors_recommendation.value_counts(dropna=False).reindex([1,np.nan,0]).to_dict()
                 }
-                if not os.path.exists("app/data/opinions"):
-                    os.mkdir("app/data/opinions")
-                with open(f"app/data/opinions/{product_id}.json","w",encoding="UTF-8") as jf:
-                    json.dump(statistics, jf, indent=4, ensure_ascii = False)
+                if not os.path.exists("app/data/statistics"):
+                    os.mkdir("app/data/statistics")
+                with open(f"app/data/statistics/{product_id}.json", "w", encoding="UTF-8") as jf:
+                    json.dump(statistics, jf, indent=4, ensure_ascii=False)
                 return redirect(url_for('product', product_id=product_id))
             return render_template("extract.html", error = "Product has no opinions")
-        return render_template("extract.html", error="Product does not exist")
+        return render_template("extract.html", error = "Product does not exist")
     return render_template("extract.html")
 
 @app.route("/products")
